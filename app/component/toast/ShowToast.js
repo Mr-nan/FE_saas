71bada2d1c0f208cb03a3ce58a4a62d3b4de3667
zhaojian <zhaojian@dycd.com>
2017-01-16 15:12:15 +0800

    新增toast组件 将现有地址增加到appurls文件中

---
+++ app/component/toast/ShowToast.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import Toast from './Toast';
+import Confirm from './Confirm';
+/**
+ * from @zhaojian
+ *
+ * 显示弹框和吐司组件   调用时使用ShowToast组件。
+ * 需要显示吐司时，调用changeType方法，传递参数为本类TOAST静态常量，msg通过props传递。
+ * 需要显示确认弹框时，调用changeType方法，传递参数为本类CONFIRM静态常量，
+ * 有msg、title、leftText、rightText、leftCallBack、rightCallBack属性通过props传递
+**/
+export default  class ShowToast extends Component {
+    static TOAST = "1";
+    static CONFIRM = "2";
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    _toastOnPress = () => {
+        this.refs.toast.changeType(this.state.toastType);
+        this.refs.toast.open();
+        this.timer = setTimeout(() => {
+
+        }, 1000);
+    }
+
+    _confirmOnPress = () => {
+        this.refs.confirm.open();
+    }
+
+
+    changeType = (_type) => {
+        if (_type === ShowToast.TOAST) {
+            this._toastOnPress();
+        } else if(_type === ShowToast.CONFIRM){
+            this._confirmOnPress();
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Toast ref='toast' type="" msg={this.props.msg}></Toast>
+                <Confirm ref='confirm' leftFunc={this.props.leftCallBack} rightFunc={this.props.rightCallBack}
+                         btnLeftText={this.props.leftText}
+                         btnRightText={this.props.rightText} title={this.props.title} msg={this.props.msg}></Confirm>
+            </View>
+        )
+    }
+}
