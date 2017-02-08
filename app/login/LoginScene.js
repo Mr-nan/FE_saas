b9037f83fb24aca3db6afb1ee6d1eec79912524f
zhaojian <zhaojian@dycd.com>
2017-02-08 17:39:01 +0800

    新增本地存储util 新增常量类 新增button组件封装

--- app/login/LoginScene.js
+++ app/login/LoginScene.js
@@ -10,16 +10,8 @@ import BaseComponent from '../component/BaseComponent';
 import SGListView from 'react-native-sglistview';
 
 export default class LoginScene extends BaseComponent {
+    initFinish = () => {
 
-
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-            ])
-        };
     }
 
     onPress = () => {
@@ -29,14 +21,7 @@ export default class LoginScene extends BaseComponent {
     render() {
         return (
             <View style={{backgroundColor: '#668800', flex: 1, paddingTop: 20}}>
-                <SGListView
-                    dataSource={this.state.dataSource} //data source
-                    ref={'listview'}
-                    initialListSize={10}
-                    renderRow={(item) =>
-                        <Text>{item}</Text>
-                    }
-                />
+
             </View>
         );
     }
