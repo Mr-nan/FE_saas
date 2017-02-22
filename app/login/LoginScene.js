a957129e550a29fb1095a388aae0c45cc94a85d5
huangning <huangning@icyd.com>
2017-02-22 20:25:53 +0800

    手势解锁实现

--- app/login/LoginScene.js
+++ app/login/LoginScene.js
@@ -91,7 +91,6 @@ export default class LoginScene extends BaseComponent {
                         }}
                     />
 
-
                     <View style={styles.inputTextSytle}>
                         <LoginAutoSearchInputText
                             ref="loginUsername"
