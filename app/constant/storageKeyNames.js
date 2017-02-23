2d7dfefc46dcc8b941ccc064c33ad7993ff3eccd
huangning <huangning@icyd.com>
2017-02-23 18:44:14 +0800

    登录 功能完善

--- app/constant/storageKeyNames.js
+++ app/constant/storageKeyNames.js
@@ -2,7 +2,8 @@ export const ISLOGIN = "islogin";//标识用户是否登录
 export const CAR_SEARCH = "car_search";
 export const CITY_SEARCH = "city_search";
 export const USER_INFO = "user_info";//保存用户信息
-export const GESTURE = "gesture"
+export const GESTURE = "gesture";
+export const USERNAME = "USERNAME";
 
 /* userinfo start */
 export const base_user_id = "base_user_id";//用户ID
