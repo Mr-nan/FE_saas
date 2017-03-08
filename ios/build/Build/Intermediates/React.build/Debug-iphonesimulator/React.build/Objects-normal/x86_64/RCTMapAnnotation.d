b6db02cd341b70120bb62864d5a4b15ffc532dfe
lianghongchao <lianghongchao@dycd.com>
2017-03-08 18:38:59 +0800

    Remove the now ignored directory some-directory

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTMapAnnotation.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-<<<<<<< HEAD
-  /Users/yujinzhong/Desktop/FE_Saas/node_modules/react-native/React/Views/RCTMapAnnotation.m \
-  /Users/yujinzhong/Desktop/FE_Saas/node_modules/react-native/React/Views/RCTMapAnnotation.h
-=======
-  /Users/lhc/Desktop/FE_Saas/node_modules/react-native/React/Views/RCTMapAnnotation.m \
-  /Users/lhc/Desktop/FE_Saas/node_modules/react-native/React/Views/RCTMapAnnotation.h
->>>>>>> 69e1a1af155b930861b1abbc6981d1889051a72f
