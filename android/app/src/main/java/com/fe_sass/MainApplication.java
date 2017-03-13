1c9d32e43d810dbe09812de02c0c333d148be174
huangning <huangning@icyd.com>
2017-03-13 16:42:22 +0800

    冲突解决

--- android/app/src/main/java/com/fe_sass/MainApplication.java
+++ android/app/src/main/java/com/fe_sass/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.theweflex.react.WeChatPackage;
-import com.liuchungui.react_native_umeng_push.UmengPushPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -37,8 +35,6 @@ public class MainApplication extends UmengPushApplication implements ReactApplic
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new WeChatPackage(),
-            new UmengPushPackage(),
           new SQLitePluginPackage(),
           new UmengPushPackage(),
           new ImagePickerPackage(),
