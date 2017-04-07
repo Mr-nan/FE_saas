67ab6f5dc516b371af54eee5d2c46b48f1941f6d
lhc <lianghongchao@dycd.com>
2017-04-07 18:23:46 +0800

    借款页面车辆列表

--- app/constant/appUrls.js
+++ app/constant/appUrls.js
@@ -1,18 +1,18 @@
 /**
  * 开发地址
  */
-// export const BASEURL = 'http://dev.api-gateway.dycd.com/';
+export const BASEURL = 'http://dev.api-gateway.dycd.com/';
 
 /**
  * 测试地址
  */
-export const BASEURL = 'http://api-gateway.test.dycd.com/';
+// export const BASEURL = 'http://api-gateway.test.dycd.com/';
 
 
 /**
  * 正式地址
  */
-export const BASEURL = 'http://gatewayapi.dycd.com/';
+// export const BASEURL = 'http://gatewayapi.dycd.com/';
 
 
 /**
