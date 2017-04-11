c40d1c949dfd7e29c95388ed27cf8a204ccab15f
lhc <lianghongchao@dycd.com>
2017-04-11 10:19:34 +0800

    OBD 扫描

---
+++ ios/OCRSCancer/QrScan.h
@@ -0,0 +1,14 @@
+//
+//  QrScan.h
+//  FE_Sass
+//
+//  Created by lcus on 17/4/11.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+@interface QrScan : NSObject<RCTBridgeModule>
+
+@end
