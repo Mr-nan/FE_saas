94d88e905a00642c13969f82d24b6b72f071b9eb
zhaojian <zhaojian@dycd.com>
2017-04-17 14:15:55 +0800

    'weixinfenxjicheng'

--- app/constant/appUrls.js
+++ app/constant/appUrls.js
@@ -6,12 +6,12 @@
 /**
  * 测试地址
  */
-export const BASEURL = 'http://api-gateway.test.dycd.com/';
+// export const BASEURL = 'http://api-gateway.test.dycd.com/';
 
 /**
  * 正式地址
  */
-// export const BASEURL = 'https://gatewayapi.dycd.com/';
+export const BASEURL = 'https://gatewayapi.dycd.com/';
 
 
 /**
