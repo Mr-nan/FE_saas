fe24c672f0cfa516146fc507185ff203f0978cf1
liusai <liusai@dycd.com>
2017-04-19 15:18:09 +0800

    压缩照片优化

--- android/react-native-custom-camera/src/main/java/com/custom/camera/CustomCameraModule.java
+++ android/react-native-custom-camera/src/main/java/com/custom/camera/CustomCameraModule.java
@@ -94,13 +94,14 @@ public class CustomCameraModule extends ReactContextBaseJavaModule implements Ac
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        int options = 100;
+        int options = 60;
 
-        while (baos.toByteArray().length / 1024 > 100) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
+        while (baos.toByteArray().length / 1024 > 600) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
             baos.reset();// 重置baos即清空baos
             image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
             if(options == 0) break;
             options -= 10;// 每次都减少10
+            Log.e("==========>>>>>",options + "===" + baos.toByteArray().length / 1024);
         }
 
         byte[] bytes = baos.toByteArray();
