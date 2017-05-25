44329ed4224fc8decc3701e561cd87aa4d7a326b
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-05-25 15:44:30 +0800

    ceshi

--- app/constant/webBackUrl.js
+++ app/constant/webBackUrl.js
@@ -3,52 +3,52 @@
  *
  * 个人账户开户
  **/
-export const OPENINDIVIDUALACCOUNT = 'http://www.openindividualaccount.com';
+export const OPENINDIVIDUALACCOUNT = 'www.openindividualaccount.com';
 /**
  * from @zhaojian
  *
  * 企业账户开户
  **/
-export const OPENENTERPRISEACCOUNT = 'http://www.openenterpriseaccount.com';
+export const OPENENTERPRISEACCOUNT = 'www.openenterpriseaccount.com';
 /**
  * from @zhaojian
  *
  * 绑定银行卡
  **/
-export const BINDCARD = 'http://www.bindcard.com';
+export const BINDCARD = 'www.bindcard.com';
 /**
  * from @zhaojian
  *
  * 转账
  **/
-export const TRANSFER = 'http://www.transfer.com';
+export const TRANSFER = 'www.transfer.com';
 /**
  * from @zhaojian
  *
  * 解绑银行卡
  **/
-export const UNBINDCARD = 'http://www.unbindcard.com';
+export const UNBINDCARD = 'www.unbindcard.com';
 /**
  * from @zhaojian
  *
  * 修改交易密码
  **/
-export const CHANGEPWD = 'http://www.changepwd.com';
+export const CHANGEPWD = 'www.changepwd.com';
 /**
  * from @zhaojian
  *
  * 重置交易密码
  **/
-export const RESETPWD = 'http://www.resetpwd.com';
+export const RESETPWD = 'www.resetpwd.com';
 /**
  * from @zhaojian
  *
  * 修改手机号
  **/
-export const CHANGEPHONE = 'http://www.changephone.com';
+export const CHANGEPHONE = 'www.changephone.com';
 /**
  * from @zhaojian
  *
  * 提现
  **/
-export const WITHDRAWALS = 'http://www.withdrawals.com';
+export const WITHDRAWALS = 'www.withdrawals.com';
