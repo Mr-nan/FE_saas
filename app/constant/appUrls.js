c6e8c7e0594b7963ec3a6b0cd4043fc39c68ef8a
zhengnan <zhengnan@zhengnandeiMac.local>
2017-05-31 17:38:06 +0800

    no message

--- app/constant/appUrls.js
+++ app/constant/appUrls.js
@@ -118,6 +118,11 @@ export const CAR_GET_REFERENCEPRICE = BASEURL + V + 'che/referencePrice';
 export const CAR_GET_RESIDUALS = BASEURL + V + 'che/residuals';
 
 /**
+ * 车辆下订单
+ */
+export const CAR_ORDER_SAVE = BASEURL + V + 'order/save';
+
+/**
  * 获取客服电话号码
  */
 export const CAR_CUSTOMER_PHONE_NUMBER = BASEURL + V + '/index/getCustomService';
