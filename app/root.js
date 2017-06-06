35fd3792b8d9b6715a1e2fd43b63255439626b56
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-06-06 19:08:25 +0800

    test

--- app/root.js
+++ app/root.js
@@ -49,4 +49,7 @@ export default class root extends Component {
     showModal = (value) => {
         this.refs.toast.showModal(value);
     }
+    showToast = (test) => {
+        this.refs.toast.changeType(ShowToast.TOAST, content);
+    }
 }
