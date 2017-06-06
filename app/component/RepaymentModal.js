fba0d57cb96c8d7691a8f86917acf00de93531de
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-06-06 11:07:38 +0800

    xiugai

--- app/component/RepaymentModal.js
+++ app/component/RepaymentModal.js
@@ -13,7 +13,7 @@ let Pixel = new PixelUtil();
 let Dimensions = require('Dimensions');
 let {width, height} = Dimensions.get('window');
 import  * as fontAndColor from '../constant/fontAndColor';
-export default class AdjustModal extends Component {
+export default class RepaymentModal extends Component {
 
     constructor(props) {
         super(props);
@@ -41,17 +41,11 @@ export default class AdjustModal extends Component {
                 visible={this.state.isShow}
                 onRequestClose={() => {}}
             >
-                <TouchableOpacity
-                    onPress={()=>{
-                       this.setState({
-                            isShow: false
-                        });
-                    }}
-                    activeOpacity={1}
+                <View
                     style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'rgba(0,0,0,0.3)'}}>
-                    <View style={{width:width-width/4,height:Pixel.getPixel(155),backgroundColor:'#fff',
+                    <View style={{width:width-width/4,height:Pixel.getPixel(175),backgroundColor:'#fff',
                      paddingLeft:Pixel.getPixel(20),paddingRight:Pixel.getPixel(20),justifyContent:'center',alignItems: 'center'}}>
-                        <Text style={{fontSize:Pixel.getPixel(17),fontWeight: 'bold',color:'#000'}}>使用规则</Text>
+                        <Text style={{fontSize:Pixel.getPixel(17),fontWeight: 'bold',color:'#000'}}>提示</Text>
                         <Text style={{textAlign: 'center',fontSize:Pixel.getPixel(14),
                          marginTop: Pixel.getPixel(11),color:'#000'}}>
                             {this.state.content}
@@ -60,13 +54,14 @@ export default class AdjustModal extends Component {
                              this.setState({
                             isShow: false
                         });
+                           this.props.callBack();
                         }} activeOpacity={0.9} style={{width:width-width/4-Pixel.getPixel(40),height:Pixel.getPixel(35),
                          marginTop:Pixel.getPixel(16),flexDirection:'row',
                          justifyContent:'center',alignItems:'center',borderRadius:3,borderWidth:1,borderColor:fontAndColor.COLORB0}}>
-                             <Text style={{fontSize: Pixel.getPixel(fontAndColor.LITTLEFONT28),color: fontAndColor.COLORB0}}>我知道了</Text>
+                             <Text style={{fontSize: Pixel.getPixel(fontAndColor.LITTLEFONT28),color: fontAndColor.COLORB0}}>确定</Text>
                         </TouchableOpacity>
                     </View>
-                </TouchableOpacity>
+                </View>
             </Modal>
         );
     }
