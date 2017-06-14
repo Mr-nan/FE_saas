5f9ee9bed2158278f17901cab9284c16fd796683
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-06-14 12:53:57 +0800

    ceshi

--- app/main/RootScene.js
+++ app/main/RootScene.js
@@ -29,14 +29,43 @@ var Pixel = new PixelUtil();
 import codePush from 'react-native-code-push'
 const versionCode = 14.0;
 let canNext = true;
-
+let Platform = require('Platform');
+let deploymentKey = '';
 export default class RootScene extends BaseComponent {
 
     componentDidMount() {
-        codePush.sync();
-        AppState.addEventListener("change", (newState) => {
-            newState === "active" && codePush.sync();
+        // codePush.sync();
+        // AppState.addEventListener("change", (newState) => {
+        //     newState === "active" && codePush.sync();
+        // });
+        if(Platform.OS === 'android'){
+            deploymentKey='fSQnzvsEP5qb9jD_tr4k2QC9pKlie1b7b22b-ea3f-4c77-abcc-72586c814b3c';
+        }else{
+            deploymentKey='TXKA_1RB5rKvXMOuBTMqPoon2c5Pe1b7b22b-ea3f-4c77-abcc-72586c814b3c';
+        }
+        codePush.checkForUpdate(deploymentKey).then((update) => {
+            if (!update) {
+            } else {
+                codePush.sync({deploymentKey: deploymentKey,
+                    updateDialog: {
+                        optionalIgnoreButtonLabel: '稍后',
+                        optionalInstallButtonLabel: '立即更新',
+                        optionalUpdateMessage: '请更新到最新版本',
+                        title: '更新提示'
+                    },
+                    installMode: codePush.InstallMode.IMMEDIATE,
+                }, (status) => {
+                    switch (status) {
+                        case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+                            break;
+                        case codePush.SyncStatus.INSTALLING_UPDATE:
+                            break;
+                    }
+                }, (progress) => {
+                });
+            }
         });
+
         BackAndroid.addEventListener('hardwareBackPress', this.handleBack);
         InteractionManager.runAfterInteractions(() => {
             this.setState({renderPlaceholderOnly: 'loading'});
@@ -57,7 +86,7 @@ export default class RootScene extends BaseComponent {
                         this.toNextPage(this.navigatorParams);
                     } else {
                         this.toJump();
-                     }
+                    }
                 },
                 (error) => {
                     this.toJump();
