40462269460322bb0f9849bf1896972d59d0ec2f
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-06-26 19:33:22 +0800

    xiugaipeizhiweibaoxinxitiyan

--- app/carSource/CarInfoScene.js
+++ app/carSource/CarInfoScene.js
@@ -544,7 +544,8 @@ export default class CarInfoScene extends BaseComponent {
             name: "CarUpkeepScene",
             component: CarUpkeepScene,
             params: {
-                    vin:vin
+                vin:vin,
+                carData:this.state.carData
             }
         }
         this.toNextPage(navigationParams);
