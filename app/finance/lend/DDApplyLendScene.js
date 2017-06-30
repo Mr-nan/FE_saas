b2ce1c4b332ceade742973dcc60910c2d96a60d4
huangning <huangning@icyd.com>
2017-06-30 11:55:00 +0800

    订单融资

--- app/finance/lend/DDApplyLendScene.js
+++ app/finance/lend/DDApplyLendScene.js
@@ -220,15 +220,7 @@ export default class DDApplyLendScene extends BaseComponent {
             })
             dataSource['section3'] = tempCarDate;
             let section4;
-            if (carData[0].is_mortgagor == 1 || carData[0].is_new == 1) {
-                section4 = [
-                    {
-                        title: 'OBD设备',
-                        key: this.OBDtransferToString(carData[0].obd_audit_status, carData[0].obd_bind_status)
-                    },
-
-                ]
-            } else {
+            if (carData[0].is_mortgagor == 0 || carData[0].is_new == 1) {
                 section4 = [
                     {
                         title: 'OBD设备',
@@ -237,6 +229,14 @@ export default class DDApplyLendScene extends BaseComponent {
                     {
                         title: '车辆权属',
                         key: this.OwnershiptransferToString(carData[0].auto_ownership_status, carData[0].order_ownership_status)
+                    },
+
+                ]
+            } else {
+                section4 = [
+                    {
+                        title: 'OBD设备',
+                        key: this.OBDtransferToString(carData[0].obd_audit_status, carData[0].obd_bind_status)
                     },
 
                 ]
@@ -439,7 +439,7 @@ export default class DDApplyLendScene extends BaseComponent {
                 if (this.carData.obd_bind_status == 0) {
                     this.props.showToast("0BD未绑定");
                 } else if (this.carData.obd_bind_status == 1) {
-                    if (this.carData.is_mortgagor == 1 || this.carData.is_mortgagor == 1) {
+                    if (this.carData.is_new == 1 || this.carData.is_mortgagor == 0) {
                         if (this.carData.order_ownership_status == 1) {
                             this.lendMoneyClick();
                         } else {
@@ -453,7 +453,7 @@ export default class DDApplyLendScene extends BaseComponent {
                 }
             } else {
                 if (this.carData.obd_audit_status == 0) {
-                    if (this.carData.is_mortgagor == 1 || this.carData.is_mortgagor == 1) {
+                    if (this.carData.is_new == 1 || this.carData.is_mortgagor == 0) {
                         if (this.carData.auto_ownership_status == 0) {
                             this.lendMoneyClick();
                         } else if (this.carData.auto_ownership_status == 1) {
@@ -465,9 +465,7 @@ export default class DDApplyLendScene extends BaseComponent {
                         this.lendMoneyClick();
                     }
                 } else if (this.carData.obd_audit_status == 1) {
-                    if (this.carData.is_mortgagor == 1 || this.carData.is_mortgagor == 1) {
-                        this.lendMoneyClick();
-                    } else {
+                    if (this.carData.is_new == 1 || this.carData.is_mortgagor == 0) {
                         if (this.carData.auto_ownership_status == 0) {
                             this.lendMoneyClick();
                         } else if (this.carData.auto_ownership_status == 1) {
@@ -475,6 +473,8 @@ export default class DDApplyLendScene extends BaseComponent {
                         } else if (this.carData.auto_ownership_status == 2) {
                             this.props.showToast("车辆权属审核未通过");
                         }
+                    } else {
+                        this.lendMoneyClick();
                     }
                 } else if (this.carData.obd_audit_status == 2) {
                     this.props.showToast("0BD审核未通过");
