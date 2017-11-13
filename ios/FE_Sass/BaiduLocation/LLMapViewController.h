faf02c267d3836b541434bd40e511d810465a8e9
余金忠 <>
2017-11-13 14:40:32 +0800

    hehe

--- ios/FE_Sass/BaiduLocation/LLMapViewController.h
+++ ios/FE_Sass/BaiduLocation/LLMapViewController.h
@@ -24,7 +24,7 @@
 
 #import <BaiduMapAPI_Radar/BMKRadarComponent.h>//引入周边雷达功能所有的头文件
 
-#import <BaiduMapAPI_Map/BMKMapView.h>//只引入所需的单个头文件
+
 
 typedef void (^LocateResultInfoBlock)(NSDictionary* result,NSString* error);
 
