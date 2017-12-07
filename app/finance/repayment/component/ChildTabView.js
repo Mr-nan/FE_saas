48fbd08b309842aab35bc3445143f723c580f609
zhengnan <zhengnan@zhengnandeiMac.local>
2017-12-07 14:29:42 +0800

    no message

--- app/finance/repayment/component/ChildTabView.js
+++ app/finance/repayment/component/ChildTabView.js
@@ -1,4 +1,4 @@
-import React, {Component, PureComponent} from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
@@ -9,17 +9,14 @@ import * as  fontAndColor from '../../../constant/fontAndColor';
 import PixelUtil from '../../../utils/PixelUtil';
 const Pixel = new PixelUtil();
 // var tabName = ["单车融资","库存融资","采购融资"];
-export default class ChildTabView extends PureComponent {
+export default class ChildTabView extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            tabName: this.props.tabName
-        }
+
     }
 
     render() {
-        let that = this;
         return (<TouchableOpacity onPress={()=>{
             this.props.goToPages(this.props.i)
         }} style={styles.tab}>
@@ -28,7 +25,7 @@ export default class ChildTabView extends PureComponent {
                 <Text allowFontScaling={false} ref="ttt"
                       style={[this.props.activeTab === this.props.i ? {color: fontAndColor.COLORB0} : {color: fontAndColor.COLORA0},
                         {fontSize: Pixel.getFontPixel(fontAndColor.LITTLEFONT28)}]}>
-                    {that.state.tabName[this.props.i]}
+                    {this.props.tabName[this.props.i]}
                 </Text>
             </View>
             { this.props.activeTab === this.props.i ?
