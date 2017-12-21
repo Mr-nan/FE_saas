96165b628d685e0271b70d2386b04dd0c6784f98
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2017-12-21 21:22:27 +0800

    shangxianqianhebing

--- app/main/RootScene.js
+++ app/main/RootScene.js
@@ -30,7 +30,7 @@ import codePush from 'react-native-code-push'
 import SQLiteUtil from "../utils/SQLiteUtil";
 import PromotionScene from "./PromotionScene";
 const SQLite = new SQLiteUtil();
-const versionCode = 28.0;
+const versionCode = 29.0;
 let canNext = true;
 let Platform = require('Platform');
 let deploymentKey = '';
@@ -121,22 +121,22 @@ export default class RootScene extends BaseComponent {
         //如果获取模拟器错误日志，需将下面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
 
 
-        // ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
-        //     this.props.showToast('' + e);
-        //     StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
-        //         let maps = {
-        //             phone: data.result,
-        //             message: '' + e
-        //         };
-        //         request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
-        //             .then((response) => {
-        //
-        //                 },
-        //                 (error) => {
-        //                 });
-        //     });
-        //
-        // });
+        ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
+            this.props.showToast('' + e);
+            StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
+                let maps = {
+                    phone: data.result,
+                    message: '' + e
+                };
+                request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
+                    .then((response) => {
+
+                        },
+                        (error) => {
+                        });
+            });
+
+        });
 
         //如果获取模拟器错误日志，需将上面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
 
