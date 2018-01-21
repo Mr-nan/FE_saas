c9322b7b1813b53120e525f988668af23161c2c5
DominicHan <120493083@qq.com>
2018-01-21 13:05:42 +0800

    订单详情字段调整

--- app/mine/myOrder/ProcurementOrderDetailScene.js
+++ app/mine/myOrder/ProcurementOrderDetailScene.js
@@ -1571,7 +1571,7 @@ export default class ProcurementOrderDetailScene extends BaseComponent {
                         }
                         this.stateMapping(status, cancelStatus);
                         this.financeInfo = this.orderDetail.finance_data;
-                        this.ordersTrans = this.orderDetail.orders_trans[0];
+                        this.ordersTrans = this.orderDetail.orders_trans_data[0];
                         //console.log(this.ordersTrans);
                         this.initListData(this.orderState);
                         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
