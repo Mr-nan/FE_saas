c4711fc5e3ca745ebdcf1aff65beb81e35afffae
zhengnan <zhengnan@zhengnandeiMac.local>
2018-03-05 19:47:21 +0800

    运单查询页，完成！

--- app/component/MyNavigator.js
+++ app/component/MyNavigator.js
@@ -7,7 +7,8 @@ import {
     TouchableHighlight
 } from 'react-native';
 var Platform = require('Platform');
-import RootScene from '../main/RootScene';
+// import RootScene from '../main/RootScene';
+import RootScene from '../mine/myOrder/CarriagePriceInfoScene';
 import {setAll} from '../constant/AllBackLogin';
 export default class MyNavigator extends Component {
     render() {
