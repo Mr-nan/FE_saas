a671f2e72b98ff6a495d50586efd5fd74741703e
赵健 <zhaojian@zhaojiandeMacBook-Pro.local>
2018-03-07 16:34:09 +0800

    xiugai

--- app/main/RootScene.js
+++ app/main/RootScene.js
@@ -37,8 +37,8 @@ let deploymentKey = '';
 import ErrorUtils from "ErrorUtils"
 import UmengPush from 'react-native-umeng-push';
 import YaoQingDeHaoLi from '../mine/setting/YaoQingDeHaoLi';
-import LoginGesture from '../mine/myOrder/LogisticsQueryScene';
-// import LoginGesture from '../login/LoginGesture';
+// import LoginGesture from '../mine/myOrder/LogisticsQueryScene';
+import LoginGesture from '../login/LoginGesture';
 const IS_ANDROID = Platform.OS === 'android';
 
 export default class RootScene extends BaseComponent {
@@ -123,22 +123,22 @@ export default class RootScene extends BaseComponent {
         //如果获取模拟器错误日志，需将下面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
 
 
-        // ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
-        //     this.props.showToast('' + e);
-        //     StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
-        //         let maps = {
-        //             phone: data.result,
-        //             message: '' + e
-        //         };
-        //         request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
-        //             .then((response) => {
-        //
-        //                 },
-        //                 (error) => {
-        //                 });
-        //     });
-        //
-        // });
+        ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
+            this.props.showToast('' + e);
+            StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
+                let maps = {
+                    phone: data.result,
+                    message: '' + e
+                };
+                request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
+                    .then((response) => {
+
+                        },
+                        (error) => {
+                        });
+            });
+
+        });
 
         //如果获取模拟器错误日志，需将上面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
 
