92096cb45f76c085622a867310a0e2ec38e289cf
xujiaqi <xujiaqi@dycd.com>
2018-03-28 12:46:23 +0800

    取消修复

--- app/finance/repayment/CancelRepayment.js
+++ app/finance/repayment/CancelRepayment.js
@@ -153,7 +153,7 @@ export  default class CancelRepayment extends BaseComponent {
                             this.backPage();
                             this.props.refreshListPage();
                         },
-                        400
+                        1000
                     );
                 }
             },(error)=>{
@@ -164,7 +164,7 @@ export  default class CancelRepayment extends BaseComponent {
                         this.backPage();
                         this.props.refreshListPage();
                 },
-                    400
+                    1000
                 );
 
             });
