a6c15b407593e0dd74631338c549180a203c6a53
huangning <huang@huangningdeMacBook-Pro.local>
2018-05-23 15:33:48 +0800

    Signed-off-by: huangning <huang@huangningdeMacBook-Pro.local>

--- app/mine/shuchebaozhengjin/SCBZJScene.js
+++ app/mine/shuchebaozhengjin/SCBZJScene.js
@@ -57,15 +57,15 @@ export  default class SCBZJScene extends BaseComponent {
                         let total = 0;
                         let left= 0;
                         let right = 0;
-                        let status = 3;
+                        let status = -1;
                         if (response.mjson.data != null) {
                             response.mjson.data[315].map((item) => {
                                 if(item.account_type_id == 11){//赎车保证金
                                     left = parseFloat(left) + parseFloat(item.balance)
-                                    // status = item.status;
+                                    status = item.status;
                                 } else if(item.account_type_id == 2){ //保证金
                                     right = parseFloat(right) + parseFloat(item.balance)
-                                    // status = item.status;
+                                    status = item.status;
                                 }
                             })
                             total = parseFloat(left) + parseFloat(right);
