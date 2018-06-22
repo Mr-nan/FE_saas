56946c045977a7c4b5040b27a9aab5759ba4244f
zhengnan <>
2018-06-22 18:20:07 +0800

    no message

--- app/constant/appUrls.js
+++ app/constant/appUrls.js
@@ -8,7 +8,7 @@
  * 测试地址
  *
  */
-export const BASEURL = 'http://api-gateway.test.dycd.com/';
+// export const BASEURL = 'http://api-gateway.test.dycd.com/';
 
 
 /**
@@ -19,7 +19,7 @@ export const BASEURL = 'http://api-gateway.test.dycd.com/';
 /**
  * 正式地址
  */
-//export const BASEURL = 'https://gatewayapi.dycd.com/';
+export const BASEURL = 'https://gatewayapi.dycd.com/';
 
 
 
