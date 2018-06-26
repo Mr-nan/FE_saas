a0cd52ca782920a099ed4153e9fce3bde7818709
郑南 <www.408972897@qq.com>
2018-06-26 22:46:55 +0800

    no message

---
+++ app/FLatListDemo.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import {
+    FlatList,
+    View,
+    Text,
+    TouchableHighlight,
+    Platform,
+    StyleSheet,
+} from 'react-native'
+
+export default class FLatListDemo extends Component {
+
+    constructor(prpos)
+    {
+        super(prpos);
+        this.state={
+            listDataSource:[
+                {t:"yang创建任务"},{:'tt'},
+                {t: '如何动态控制item的高度高度高度高度高度高度高度高度高度高度高度高度'}]
+        }
+    }
+
+    ListHeaderView(){
+        return(
+            <View style={{backgroundColor:'white'}}>
+                <Text style={[styles.headerTitle,{top:15}]}>指派人:</Text>
+                <Text style={[styles.headerText,{top:15}]}>yang:</Text>
+                <Text style={[styles.headerTitle,{top:40}]}>审核人:</Text>
+                <Text style={[styles.headerText,{top:40}]}>yang:</Text>
+                <Text style={[styles.headerTitle,{top:65}]}>任务标题:</Text>
+                <Text style={[styles.headerText,{top:65}]}>我的任务</Text>
+                <Text style={[styles.headerTitle,{top:90}]}>任务描述</Text>
+                <Text style={[styles.headerText,{top:90}]}>任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述.......</Text>
+            </View>
+        )
+    }
+
+    ListCell(itemData)
+    {
+        /*style={styles.cellText}*/
+        return(
+            <View >
+                <Text style={styles.cellText}>
+                    任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述任务描述......
+                </Text>
+            </View>
+        )
+    }
+
+    render(){
+        return(
+            <FlatList style={{flex:1,backgroundColor:'#d2d2d2',top:50}}
+                      ListHeaderComponent={this.ListHeaderView}
+                      data={this.state.listDataSource}
+                      renderItem={this.ListCell}/>
+        )
+    }
+}
+
+let styles = StyleSheet.create({
+    headerTitle:{
+        position:'absolute',
+        left:15,
+        color:"#d2d2d2",
+        width:100,
+    },
+
+    headerText:{
+        position:'absolute',
+        left:100,
+    },
+    cellText:{
+        left:15,
+        top:15,
+        width:400,
+    }
+});
+
