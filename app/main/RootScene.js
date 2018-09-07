558dc7227ffc2595b089e67cdd482fb30ccc7d11
zhengnan <>
2018-09-07 10:07:55 +0800

    no message

--- app/main/RootScene.js
+++ app/main/RootScene.js
@@ -30,7 +30,7 @@ import codePush from 'react-native-code-push'
 import SQLiteUtil from "../utils/SQLiteUtil";
 import PromotionScene from "./PromotionScene";
 const SQLite = new SQLiteUtil();
-const versionCode = 49.0;
+const versionCode = 50.0;
 let canNext = true;
 let Platform = require('Platform');
 let deploymentKey = '';
@@ -120,24 +120,24 @@ export default class RootScene extends BaseComponent {
         // //如果获取模拟器错误日志，需将下面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
         //
 
-        // ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
-        //     this.props.showToast('' + e);
-        //     StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
-        //
-        //         if(data.code != 1 || !data.result) return;
-        //         let maps = {
-        //             phone: data.result,
-        //             message: '' + e
-        //         };
-        //         request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
-        //             .then((response) => {
-        //
-        //                 },
-        //                 (error) => {
-        //                 });
-        //     });
-        //
-        // });
+        ErrorUtils.setGlobalHandler((e) => {　//发生异常的处理方法,当然如果是打包好的话可能你找都找不到是哪段代码出问题了
+            this.props.showToast('' + e);
+            StorageUtil.mGetItem(KeyNames.PHONE, (data) => {
+
+                if(data.code != 1 || !data.result) return;
+                let maps = {
+                    phone: data.result,
+                    message: '' + e
+                };
+                request(Urls.ADDACCOUNTMESSAGEINFO, 'Post', maps)
+                    .then((response) => {
+
+                        },
+                        (error) => {
+                        });
+            });
+
+        });
 
         //如果获取模拟器错误日志，需将上面代码屏蔽！！！！！！！！！！！！！！！！！！！！！！！
 
