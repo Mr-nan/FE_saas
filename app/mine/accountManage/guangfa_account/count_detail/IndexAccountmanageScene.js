50161a9786341bb4de8abafcd187641827863abe
zhengnan <>
2018-10-27 10:41:02 +0800

    no message

--- app/mine/accountManage/guangfa_account/count_detail/IndexAccountmanageScene.js
+++ app/mine/accountManage/guangfa_account/count_detail/IndexAccountmanageScene.js
@@ -12,7 +12,9 @@ import {
     Image,
     TouchableOpacity,
     StatusBar,
-    Dimensions
+    Dimensions,
+    ScrollView,
+    RefreshControl,
 
 }from 'react-native';
 
@@ -22,111 +24,193 @@ import BaseComponent from "../../../../component/BaseComponent";
 const Pixel = new PixelUtil();
 const {width,height} = Dimensions.get('window');
 import NavigationView from "../../../../component/AllNavigationView";
+import {request} from "../../../../utils/RequestUtil";
+import * as Urls from '../../../../constant/appUrls';
 
 export default class IndexAccountmanageScene extends BaseComponent{
     constructor(props) {
         super(props);
         this.state = {
-            renderPlaceholderOnly:'blank'
+            renderPlaceholderOnly:'blank',
+            isRefreshing:false,
+            accountData:{},
         }
     }
 
     initFinish(){
         this.setState({
-            renderPlaceholderOnly:'success'
+            renderPlaceholderOnly:'loading',
         })
+        this.loadData();
     }
+
+    loadData=()=>{
+        let maps = {
+            enter_base_ids: global.companyBaseID,
+            child_type: '1',
+            bank_id: 'gfyh'
+        };
+        request(Urls.GET_USER_ACCOUNT_DETAIL, 'Post', maps)
+            .then((response) => {
+                 console.log(response);
+                this.setState({
+                    renderPlaceholderOnly: 'success',
+                    isRefreshing: false,
+                    accountData:response.mjson.data['gfyh'][0],
+                });
+            }, (error) => {
+                this.setState({
+                    renderPlaceholderOnly: 'error',
+                    isRefreshing: false,
+                });
+                this.props.showToast(error.mjson.msg);
+            });
+    }
+
+    refreshData=()=>{
+
+        this.setState({
+            isRefreshing:true,
+        });
+        let maps = {
+            enter_base_ids: global.companyBaseID,
+            child_type: '1',
+            bank_id: 'gfyh'
+        };
+        request(Urls.GET_USER_ACCOUNT_DETAIL, 'Post', maps)
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    isRefreshing: false,
+                    accountData:response.mjson.data['gfyh'][0],
+                });
+            }, (error) => {
+                this.setState({
+                    isRefreshing: false,
+                });
+                this.props.showToast(error.mjson.msg);
+            });
+    }
+
 
     render() {
         if(this.state.renderPlaceholderOnly != 'success'){
-            this.renderPlaceholderView;
+            return(
+                <View style={{flex:1,backgroundColor:fontAndColor.COLORA3}}>
+                    {
+                        this.loadView()
+                    }
+                    <NavigationView backIconClick={this.backPage}
+                                    title='账户管理'/>
+                </View>
+            )
         }
         return (
             <View style={{flex: 1,backgroundColor:fontAndColor.COLORA3}}>
                 <StatusBar barStyle='light-content'/>
+
                 <Image source={require('../../../../../images/mine/guangfa_account/tou-bg.png')}>
-                <NavigationView backIconClick={this.backPage} title='账户管理'
-                                wrapStyle={{backgroundColor:'transparent'}} titleStyle={{color:'#ffffff'}}/>
                     <View style={{flexDirection: 'row',marginTop: Pixel.getPixel(88),alignItems:'center',justifyContent:'space-between',marginLeft:Pixel.getPixel(34),marginRight: Pixel.getPixel(30,5)}}>
                         <View style={{flexDirection:'column',alignItems:'center',width:Pixel.getPixel(130)}}>
                             <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(12),lineHeight:Pixel.getPixel(17)}}>可用余额(元)</Text>
-                            <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(22),lineHeight:Pixel.getPixel(26),fontWeight: 'bold'}}>200,000.00</Text>
+                            <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(22),lineHeight:Pixel.getPixel(26),fontWeight: 'bold'}}>{this.state.accountData.balance}</Text>
                         </View>
                         <View style={{width:Pixel.getPixel(1),height:Pixel.getPixel(20),backgroundColor:'#ffffff'}}/>
                         <View style={{flexDirection:'column',alignItems:'center',width:Pixel.getPixel(130)}}>
                             <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(12),lineHeight:Pixel.getPixel(17)}}>冻结余额(元)</Text>
-                            <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(22),lineHeight:Pixel.getPixel(26),fontWeight: 'bold'}}>50,630.00</Text>
+                            <Text style={{color:'#ffffff',backgroundColor:'transparent',fontSize:Pixel.getFontPixel(22),lineHeight:Pixel.getPixel(26),fontWeight: 'bold'}}>{this.state.accountData.frozen_balance}</Text>
                         </View>
                     </View>
                 </Image>
+                <ScrollView  style={{paddingBottom:Pixel.getPixel(44)}}
+                    refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.isRefreshing}
+                        onRefresh={this.refreshData}
+                        tintColor={[fontAndColor.COLORB0]}
+                        colors={[fontAndColor.COLORB0]}
+                    />
+                }>
                 <View style={{width:width,height:Pixel.getPixel(36),justifyContent:'center'}}>
                     <Text style={{color:fontAndColor.COLORA1,fontSize:Pixel.getFontPixel(12),marginLeft:Pixel.getPixel(15)}} allowFontScaling={false}>账户功能</Text>
                 </View>
                 <View style={{backgroundColor:'#ffffff',width:width}}>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/yinghangka.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>银行卡</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
-                    <View style={{height:Pixel.getPixel(1),marginLeft:Pixel.getPixel(15),marginRight:Pixel.getPixel(15),backgroundColor:fontAndColor.COLORA3}}/>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/jianquan.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>小额鉴权</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
-                    <View style={{height:Pixel.getPixel(1),marginLeft:Pixel.getPixel(15),marginRight:Pixel.getPixel(15),backgroundColor:fontAndColor.COLORA3}}/>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/jiaoyimingxi.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>交易明细查询</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/yinghangka.png')} title="银行卡" isShowBottomLin={true} click={this.clickSender}/>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/jianquan.png')} title="小额鉴权" isShowBottomLin={true} click={this.clickSender}/>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/jiaoyimingxi.png')} title="交易明细查询" isShowBottomLin={false} click={this.clickSender}/>
                 </View>
                 <View style={{backgroundColor:'#ffffff',width:width,marginTop:Pixel.getPixel(10)}}>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/shoujihao.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>修改预留手机号</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
-                    <View style={{height:Pixel.getPixel(1),marginLeft:Pixel.getPixel(15),marginRight:Pixel.getPixel(15),backgroundColor:fontAndColor.COLORA3}}/>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/chongzhimima.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>重置密码</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/shoujihao.png')} title="修改预留手机号" isShowBottomLin={true} click={this.clickSender}/>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/chongzhimima.png')} title="重置密码" isShowBottomLin={true} click={this.clickSender}/>
                 </View>
                 <View style={{backgroundColor:'#ffffff',width:width,marginTop:Pixel.getPixel(10)}}>
-                    <TouchableOpacity style={{paddingLeft: Pixel.getPixel(15),paddingRight: Pixel.getPixel(15),height:Pixel.getPixel(43),width:width,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-                        <View style={{flexDirection:'row',alignItems:'center'}}>
-                            <Image source={require('../../../../../images/mine/guangfa_account/shehzi.png')}/>
-                            <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>账户设置</Text>
-                        </View>
-                        <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
-                    </TouchableOpacity>
+                    <CellItem imageData={require('../../../../../images/mine/guangfa_account/shehzi.png')} title="账户设置" isShowBottomLin={true} click={this.clickSender}/>
                 </View>
-                <View style={{width:width,height:Pixel.getPixel(44),flexDirection:'row',marginTop:Pixel.getPixel(127)}}>
-                    <TouchableOpacity style={{flex:1,backgroundColor:'#ffffff',alignItems:'center',justifyContent:'center'}}>
+                </ScrollView>
+                <View style={{left:0,right:0,height:Pixel.getPixel(44),flexDirection:'row',bottom:Pixel.getBottomPixel(0),position: 'absolute'}}>
+                    <TouchableOpacity style={{flex:1,backgroundColor:'#ffffff',alignItems:'center',justifyContent:'center'}}
+                                      onPress={this.payAction}>
                         <Text style={{color:fontAndColor.COLORB0,fontSize:Pixel.getFontPixel(15)}}>提现</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={{flex:1,backgroundColor:'#ffffff',marginLeft:Pixel.getPixel(1),alignItems:'center',justifyContent:'center'}}>
+                    <TouchableOpacity style={{flex:1,backgroundColor:'#ffffff',marginLeft:Pixel.getPixel(1),alignItems:'center',justifyContent:'center'}}
+                                      onPress={this.withdrawAction}>
                         <Text style={{color:fontAndColor.COLORB0,fontSize:Pixel.getFontPixel(15)}}>充值</Text>
                     </TouchableOpacity>
                 </View>
+                <NavigationView backIconClick={this.backPage} title='账户管理'
+                                wrapStyle={{backgroundColor:'transparent'}} titleStyle={{color:'#ffffff'}}/>
             </View>
         );
     }
 
-    renderPlaceholderView = () => {
-        this.loadView();
+    clickSender=(title)=>{
+
+        console.log(title);
+
+    }
+
+
+    // 充值
+    payAction=()=>{
+
+    }
+
+    // 提现
+    withdrawAction=()=>{
+
+    }
+
+
+
+
+}
+
+class CellItem extends  Component{
+
+    render(){
+        let {imageData,title,click,isShowBottomLin} = this.props;
+        return(
+            <View>
+                <TouchableOpacity style={{
+                    paddingLeft: Pixel.getPixel(15),
+                    paddingRight: Pixel.getPixel(15),
+                    height:Pixel.getPixel(43),
+                    width:width,
+                    flexDirection:'row',
+                    justifyContent:'space-between',
+                    alignItems:'center'}} onPress={()=>{click && click(title)}}>
+                    <View style={{flexDirection:'row',alignItems:'center'}}>
+                        <Image source={imageData}/>
+                        <Text style={{marginLeft:Pixel.getPixel(18),color:fontAndColor.COLORA0,fontSize:Pixel.getFontPixel(14)}} allowFontScaling={false}>{title}</Text>
+                    </View>
+                    <Image source={require('../../../../../images/mine/guangfa_account/xiangqing.png')}/>
+
+                </TouchableOpacity>
+                {
+                    isShowBottomLin && <View style={{height:Pixel.getPixel(1),marginLeft:Pixel.getPixel(15),marginRight:Pixel.getPixel(15),backgroundColor:fontAndColor.COLORA3}}/>
+                }
+            </View>
+        )
     }
 
 }
