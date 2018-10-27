13c46baaa24ce1719e908a75e79e631e8984884d
zhengnan <>
2018-10-27 09:13:44 +0800

    no message

--- app/constant/storageKeyNames.js
+++ app/constant/storageKeyNames.js
@@ -1,5 +1,5 @@
 export const VERSON_NAME = "6.0.3"; // 版本名
-export const VERSON_CODE = "52"; // 版本号
+export const VERSON_CODE = "53"; // 版本号
 export const ISLOGIN = "islogin";//标识用户是否登录
 export const CAR_SEARCH = "car_search";
 export const CITY_SEARCH = "city_search";
