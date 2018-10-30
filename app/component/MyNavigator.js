e54eb015d6e18ddb4abfc507aea71648315b1c6e
zhengnan <>
2018-10-30 15:04:14 +0800

    no message

--- app/component/MyNavigator.js
+++ app/component/MyNavigator.js
@@ -7,8 +7,8 @@ import {
     TouchableHighlight
 } from 'react-native';
 var Platform = require('Platform');
-// import RootScene from '../main/RootScene';
-import RootScene from '../mine/accountManage/SelectBankScene';
+import RootScene from '../main/RootScene';
+// import RootScene from '../mine/accountManage/SelectBankScene';
 import {setAll} from '../constant/AllBackLogin';
 export default class  MyNavigator extends Component {
     render() {
