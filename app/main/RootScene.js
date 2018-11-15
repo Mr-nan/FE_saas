964a4af70d1105faee8cf8d602db2339c7a6c6ac
zhengnan <>
2018-11-15 15:08:58 +0800

    no message

--- app/main/RootScene.js
+++ app/main/RootScene.js
@@ -30,7 +30,7 @@ import codePush from 'react-native-code-push'
 import SQLiteUtil from "../utils/SQLiteUtil";
 import PromotionScene from "./PromotionScene";
 const SQLite = new SQLiteUtil();
-const versionCode = 53.0;
+const versionCode = 54.0;
 let canNext = true;
 let Platform = require('Platform');
 let deploymentKey = '';
